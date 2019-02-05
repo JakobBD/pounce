1806322cd22e7449cf1cdb37edbef9d310f2c446
Your Name <you@example.com>
2019-02-05 15:28:41 +0100

    uqmethod mlmc runIteration and getNodesAndWeights

--- uqmethod/uqmethod.py
+++ uqmethod/uqmethod.py
@@ -1,8 +1,24 @@
 from helpers.baseclass import BaseClass
 
-class UqMethod(BaseClass): 
+class UqMethod(BaseClass):
+  def RunSimulation(self,machine,solver):
+     """Main Loop for UQMethod.
+          General procedure:
+          1. Let machine decide how many samples to compute.
+          2. Generate samples and weights.
+          3. Compute samples on system
 
-   def RunSimulation(self,machine,solver):
-      pass
+      Args:
+          machine: class of machine, i.e. local, or computing cluster.
+          solver: class of solver, i.e. flexi, or python solver.
+
+     """
+     iteration=0
+     while True:
+       # nSamples=machine.allocateRessources(solver.dofsPerCore)
+        samples, weight=self.GetNodesAndWeights(nSamples)
+        iteration+=1
+        if(iteration==self.nMaxIter):
+           break
 
 from . import *
--- uqmethod/uqmethodmlmc.py
+++ uqmethod/uqmethodmlmc.py
@@ -1,7 +1,30 @@
+import numpy as np
 from .uqmethod import UqMethod
+from .stochparameters import StochParameters
+
 
 @UqMethod.RegisterSubclass('mlmc')
 class Mlmc(UqMethod):
-   pass
+   def GetNodesAndWeights(self,nSamples):
+       """Draws random samples for uncertain input
 
+       Args:
+          nSamples [1:nLevels]: array of number of samples to draw .
 
+       Returns:
+          samples[0:nLevels-1,0:nStochDim].
+       """
+       samples=[]
+       weights=[]
+       for idx,level in enumerate(self.levels):
+          localSamples=[]
+          localweights=[]
+          for dist in self.distribution:
+             for key in dist:
+                if(key=="uniform"):
+                   localSamples.append(np.random.uniform(dist[key][0],dist[key][1],nSamples[idx]))
+                if(key=="normal"):
+                   localSamples.append(np.random.normal(dist[key][0],dist[key][1],nSamples[idx]))
+          samples.append(localSamples)
+          weights.append(localweights)
+       return samples,weights

1113169e9c3b1dea481352ade023d8485e97cf5d
thomas <iagtkuhn@iagpc151.iag.uni-stuttgart.de>
2019-02-06 11:11:04 +0100

    solverinternal

--- src/solver/solverinternal.py
+++ src/solver/solverinternal.py
@@ -1,6 +1,30 @@
 from .solver import Solver
+import h5py
+import numpy as np
 
 @Solver.RegisterSubclass('internal')
 class SolverInternal(Solver):
+   self.projectname = "Integration"
+
    def Report(self): 
       print("I am Internal")
+
+
+   def PrepareSimulation(self,prepSimuDict):
+      self.WriteHdf5(prepSimuDict)
+      # generateRunCommand(self)
+
+   # def GenerateRunCommand(self):
+      # self.runCommand='python '+self.exePath
+
+   def WriteHdf5(self,prepSimuDict):
+      h5f = h5py.File(self.projectname+'_'+str(prepSimuDict["level"])+prepSimuDict["sublevel"]+'_StochInput.h5', 'w')
+      h5f.create_dataset('Samples', data=np.transpose(prepSimuDict["samples"][:]))
+      h5f.create_dataset('Weights', data=prepSimuDict["weights"])
+      h5f.attrs.create('StochVars', prepSimuDict["varnames"], (len(prepSimuDict["varnames"]),))
+      h5f.attrs['Level']    = prepSimuDict["level"]
+      h5f.attrs['SubLevel'] = prepSimuDict["sublevel"]
+      h5f.close()
+
+
+

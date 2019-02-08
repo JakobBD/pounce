5faa9218405bb534c9136d4be025bf146b0848a3
Your Name <you@example.com>
2019-02-08 16:24:45 +0100

    update

--- src/machine/machinecray.py
+++ src/machine/machinecray.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+import glob
 from .machine import Machine
 
 @Machine.RegisterSubclass('cray')
@@ -60,19 +62,41 @@ class Cray(Machine):
                         found=True
                         status = line.split()[9]
                         if status=='C':
-                           [jobHandles,unfinished_jobs,n_remain]=check_errorfile(i,jobHandles,unfinished_jobs,index,n_remain,do_exit)
+                           [jobHandles,unfinished_jobs,n_remain]=CheckErrorfile(i,jobHandles,unfinished_jobs,index,n_remain)
                         else:
                            status_string="id {} has status {}".format(i,status)
                            if not status_string in status_list:
                               status_list.append(status_string)
                               print("Job with {}!".format(status_string))
                if not found: # jobID is not in st: pretend it is finished
-                  [jobHandles,unfinished_jobs,n_remain]=check_errorfile(i,jobHandles,unfinished_jobs,index,n_remain,do_exit)
+                  [jobHandles,unfinished_jobs,n_remain]=CheckErrorfile(i,jobHandles,unfinished_jobs,index,n_remain)
          if sum(jobHandles)==0:
             comp_finished=True
          else:
             time.sleep(1)
       return(unfinished_jobs)
+
+   def CheckErrorfile(i,jobID,unfinished_jobs,index,n_remain):
+   with open(glob.glob('*.e{}'.format(i))[0]) as f:
+      lines = f.read().splitlines()
+      if len(lines)==0:
+         error=None
+      else:
+         error=True
+         for errortext in lines:
+            errortext=errortext.split()
+            if len(errortext)>=7:
+               if errortext[4]=='walltime' and errortext[6]=='exceeded':
+                  error=None
+                  unfinished_jobs=True
+                  break
+   if error:
+      print('\nWARNING: Error in jobfile execution! See file *.e{} for details.\n'.format(i))
+      unfinished_jobs=True
+   jobID[index]=0
+   n_remain-=1
+   print('job ID {} is now complete! {} jobs remaining'.format(i,n_remain))
+   return(jobID,unfinished_jobs,n_remain)
 
    def CheckAllFinished(self):
       """Checks if all computations finished and all files are available.

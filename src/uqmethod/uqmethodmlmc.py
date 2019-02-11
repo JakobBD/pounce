c17caff8c7670b57564b37fe261b22d6d00d7f94
Jakob Duerrwaechter <iagduerr@iagpc151.iag.uni-stuttgart.de>
2019-02-11 15:32:24 +0100

    add active levels and active sublevels to account for levels with nSamples=0

--- src/uqmethod/uqmethodmlmc.py
+++ src/uqmethod/uqmethodmlmc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import copy
 
 from .uqmethod import UqMethod
 from helpers.printtools import *
@@ -27,16 +28,18 @@ class Mlmc(UqMethod):
    def SetupLevels(self):
       self.allSublevels=[]
       for iLevel,level in enumerate(self.levels):
-         level.nFinshedSamples = 0
+         level.nFinishedSamples = 0
          level.ind=iLevel+1
          level.sublevels= [ SubLevel(level,level,'f') ]
          self.allSublevels.append(level.sublevels[-1])
          if iLevel > 0:
             level.sublevels.append(SubLevel(level,self.levels[iLevel-1],'c'))
             self.allSublevels.append(level.sublevels[-1])
+      self.activeSublevels=self.allSublevels.copy()
+      self.activeLevels=self.levels.copy()
 
    def GetNodesAndWeights(self):
-      for level in self.levels:
+      for level in self.activeLevels:
          level.samples=[]
          for var in self.stochVars:
             level.samples.append(var.DrawSamples(level.nCurrentSamples))
@@ -44,32 +47,32 @@ class Mlmc(UqMethod):
          level.weights=[]
 
    def PrepareAllSimulations(self):
-      for sublevel in self.allSublevels:
+      for sublevel in self.activeSublevels:
          furtherAttrs=sublevel.solverPrms
          furtherAttrs.update({"Level":sublevel.level.ind})
          furtherAttrs.update({"Sublevel":sublevel.subName})
          sublevel.runCommand=self.solver.PrepareSimulation(sublevel.level,self.stochVars,sublevel.wholeName,furtherAttrs)
-      for level in self.levels:
+      for level in self.activeLevels:
          del level.samples
 
    def RunAllBatches(self):
       jobHandles=[]
-      for sublevel in self.allSublevels:
+      for sublevel in self.activeSublevels:
          jobHandle = self.machine.RunBatch(sublevel.runCommand,sublevel.nCoresPerSample,self.solver)
          jobHandles.append(jobHandle)
       PrintMinorSection("Waiting for jobs to finish:")
       if self.machine.WaitFinished(jobHandles): Print("Computations finished.")
       if self.machine.CheckAllFinished(): Print("All jobs finished.")
-      for level in self.levels:
-         level.nFinshedSamples += level.nCurrentSamples
+      for level in self.activeLevels:
+         level.nFinishedSamples += level.nCurrentSamples
 
    def PrepareAllPostprocessing(self):
-      for level in self.levels:
+      for level in self.activeLevels:
          fileNameSubStr = [sublevel.wholeName for sublevel in level.sublevels]
          level.runPostprocCommand=self.solver.PreparePostprocessing(fileNameSubStr)
 
    def RunAllBatchesPostprocessing(self):
-      for level in self.levels:
+      for level in self.activeLevels:
          self.machine.RunBatch(level.runPostprocCommand,1,self.solver)
 
    def GetNewNCurrentSamples(self):
@@ -80,27 +83,31 @@ class Mlmc(UqMethod):
       sumSigmaW = 0.
       for level in self.levels:
          fileNameSubStr = str(level.ind)
-         level.sigmaSq = self.solver.GetPostProcQuantityFromFile(fileNameSubStr,"sigmaSq")
-         level.workMean = self.solver.GetPostProcQuantityFromFile(fileNameSubStr,"WorkMean")
+         if level.nCurrentSamples > 0:
+            level.sigmaSq = self.solver.GetPostProcQuantityFromFile(fileNameSubStr,"sigmaSq")
+            level.workMean = self.solver.GetPostProcQuantityFromFile(fileNameSubStr,"WorkMean")
          sumSigmaW += SafeSqrt(level.sigmaSq*level.workMean)
          stdoutTable.Update1(level)
 
       for level in self.levels:
          #TODO: add formula for given maxWork
          level.mlopt = sumSigmaW * SafeSqrt(level.sigmaSq/level.workMean) / (self.tolerance*self.tolerance/4.)
-         level.nCurrentSamples = max(int(np.ceil(level.mlopt))-level.nFinshedSamples , 0)
+         level.nCurrentSamples = max(int(np.ceil(level.mlopt))-level.nFinishedSamples , 0)
          stdoutTable.Update2(level)
 
       stdoutTable.Print()
 
+      self.activeLevels    = [i for i in self.levels if i.nCurrentSamples > 0]
+      self.activeSublevels = [i for i in self.allSublevels if i.level.nCurrentSamples > 0]
+
 
 class SubLevel():
    def __init__(self,diffLevel,resolutionLevel,name):
-      self.solverPrms=resolutionLevel.solverPrms
-      self.nCoresPerSample=resolutionLevel.nCoresPerSample
+      self.solverPrms = resolutionLevel.solverPrms
+      self.nCoresPerSample = resolutionLevel.nCoresPerSample
       self.level = diffLevel
-      self.subName=name
-      self.wholeName=str(diffLevel.ind)+name
+      self.subName = name
+      self.wholeName = str(diffLevel.ind)+name
 
 
 
@@ -111,12 +118,12 @@ class StdOutTable():
    Prints values for each level in ordered table to stdout.
    """
    def __init__(self):
-      self.headerStr   = "                ║ "
-      self.sigmaSqStr  = "        SigmaSq ║ "
-      self.meanWorkStr = "      mean work ║ "
-      self.mloptStr    = "         ML_opt ║ "
-      self.fininshedStr= "finshed Samples ║ "
-      self.newStr      = "    new Samples ║ "
+      self.headerStr   = "                 ║ "
+      self.sigmaSqStr  = "         SigmaSq ║ "
+      self.meanWorkStr = "       mean work ║ "
+      self.mloptStr    = "          ML_opt ║ "
+      self.fininshedStr= "finished Samples ║ "
+      self.newStr      = "     new Samples ║ "
 
    def Update1(self,level):
       self.headerStr  +="     Level %2d ║ "%(level.ind)
@@ -125,13 +132,13 @@ class StdOutTable():
 
    def Update2(self,level):
       self.mloptStr    +="%13.3f ║ "%(level.mlopt)
-      self.fininshedStr+="%13d ║ "%(level.nFinshedSamples)
+      self.fininshedStr+="%13d ║ "%(level.nFinishedSamples)
       self.newStr      +="%13d ║ "%(level.nCurrentSamples)
 
    def Print(self):
       Print(self.headerStr)
       sepStr="═"*14+"╬═"
-      Print("══"+sepStr*int(len(self.headerStr)/len(sepStr)))
+      Print("═══"+sepStr*int(len(self.headerStr)/len(sepStr)))
       Print(self.sigmaSqStr)
       Print(self.meanWorkStr)
       Print(self.mloptStr)

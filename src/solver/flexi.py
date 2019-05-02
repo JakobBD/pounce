ec41aad8a7e505ee4e767eaaca9c328b2a88d2dd
Jakob Duerrwaechter <iagduerr@iagpc151.iag.uni-stuttgart.de>
2019-05-02 15:14:58 +0200

    bugfix

--- src/solver/flexi.py
+++ src/solver/flexi.py
@@ -147,7 +147,7 @@ class RecordPoints(QoI):
     def prepare_iter_postproc(self,simulation):
         # participants[0] is a rather dirty hack
         self.prm_file_name = self.participants[0].prm_file_name
-        n_files = len(glob.glob(participants[0].project_name+"_RP_*.h5"))
+        n_files = len(glob.glob(self.participants[0].project_name+"_RP_*.h5"))
         self.run_command = self.exe_paths["iteration_postproc"] \
                            + " " + self.prmfiles["iteration_postproc"] \
                            + " " + self.prm_file_name

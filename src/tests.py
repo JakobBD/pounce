50fb2acdf83252ef34de2da56196f54fe0bf2679
Jakob Duerrwaechter <iagduerr@iagpc151.iag.uni-stuttgart.de>
2022-05-05 23:27:56 +0200

    adapt workflow

--- src/tests.py
+++ src/tests.py
@@ -18,21 +18,21 @@ def general_tst(prmfile,mean_ref,stddev_ref,tol):
     assert np.abs(sim.stddev - stddev_ref) < tol
 
 def test_mlmc():
-    prmfile    = "./ini/internal_local/parameter_mlmc.yml"
+    prmfile    = "../ini/internal_local/parameter_mlmc.yml"
     mean_ref   = -0.03819458024853564
     stddev_ref = 0.6905427902794208
     tol = 1.E-10
     general_tst(prmfile,mean_ref,stddev_ref,tol)
 
 def test_mfmc():
-    prmfile    = "./ini/internal_local/parameter_mfmc.yml"
+    prmfile    = "../ini/internal_local/parameter_mfmc.yml"
     mean_ref   = 0.022528934807061824
     stddev_ref = 0.691569022875092
     tol = 1.E-10
     general_tst(prmfile,mean_ref,stddev_ref,tol)
 
 def test_sc():
-    prmfile    = "./ini/internal_local/parameter_sc.yml"
+    prmfile    = "../ini/internal_local/parameter_sc.yml"
     mean_ref   = 0.2101817993546256
     stddev_ref = 0.6743135688310626
     tol = 1.E-10
